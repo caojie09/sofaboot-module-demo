8812feab68670141a5d4caa630923df809b9993c
caojie.cj <caojie.cj@antfin.com>
2018-07-17 19:17:17 +0800

    init project

---
+++ service-facade/src/main/java/com/alipay/sofa/isle/sample/SampleService.java
@@ -0,0 +1,8 @@
+package com.alipay.sofa.isle.sample;
+
+/**
+ * @author xuanbei 18/7/17
+ */
+public interface SampleService {
+    String message();
+}
---
+++ service-provide/src/main/java/com/alipay/sofa/isle/sample/SampleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alipay.sofa.isle.sample;
+
+/**
+ * @author xuanbei 18/7/17
+ */
+public class SampleServiceImpl implements SampleService {
+    private String message;
+
+    public String message() {
+        return message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
